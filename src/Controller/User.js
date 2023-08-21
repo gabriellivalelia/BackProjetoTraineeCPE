423bbafededd9c81af8ceccf11633fb57230811a
GabrielliSilva <gabriellisilva@cpejr.com.br>
2023-08-21 20:59:07 -0300

    fix: sintaxe

--- src/Controller/User.js
+++ src/Controller/User.js
@@ -1,131 +1,90 @@
 import { openDB } from "../configDB.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
+export async function createTableUser(req, res) {
+  const db = await openDB();
 
-export async function createTableUser(req,res){
-    openDB().then(db=>{
-        db.exec('CREATE TABLE IF NOT EXISTS User ( id UUID PRIMARY KEY UNIQUE NOT NULL, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, phone TEXT NOT NULL, address TEXT NOT NULL, password TEXT NOT NULL )');
-    });
+  db.exec(
+    "CREATE TABLE IF NOT EXISTS User ( id UUID PRIMARY KEY UNIQUE NOT NULL, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, phone TEXT NOT NULL, address TEXT NOT NULL, password TEXT NOT NULL )"
+  );
 }
 
-export async function createUser(req,res){
-    try {
-      let user = req.body;
-      const uuid = uuidv4();
-      const db = await openDB();
-    
-      const { userRes } = await db.run('INSERT INTO User ( id, name, email, phone, address, password) VALUES (?, ?, ?, ?, ?, ?)', [uuid, user.name, user.email, user.phone, user.address, user.password])
-    
-      return userRes;
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+export async function createUser(req, res) {
+  const userData = req.body;
+  const uuid = uuidv4();
+  const db = await openDB();
+
+  return db.run(
+    "INSERT INTO User ( id, name, email, phone, address, password) VALUES (?, ?, ?, ?, ?, ?)",
+    [
+      uuid,
+      userData.name,
+      userData.email,
+      userData.phone,
+      userData.address,
+      userData.password,
+    ]
+  );
 }
 
 export async function updateUser(req, res) {
-    try {
-        const user = req.body; // Recebe o objeto de usuário do corpo da solicitação
-        const db = await openDB(); // Abre a conexão com o banco de dados
+  const user = req.body; // Recebe o objeto de usuário do corpo da solicitação
+  const db = await openDB(); // Abre a conexão com o banco de dados
 
-        // Construir a consulta SQL apenas com os campos modificados
-        let updateQuery = 'UPDATE User SET ';
-        const updateParams = [];
-        const fieldsToUpdate = ['name', 'email', 'phone', 'address', 'password'];
+  // Construir a consulta SQL apenas com os campos modificados
+  let updateQuery = "UPDATE User SET ";
+  const updateParams = [];
+  const fieldsToUpdate = ["name", "email", "phone", "address", "password"];
 
-        // Loop pelos campos que podem ser atualizados
-        fieldsToUpdate.forEach(field => {
-            if (user[field] !== undefined) {
-                updateQuery += `${field}=?, `; // Adiciona o campo à consulta SQL
-                updateParams.push(user[field]); // Adiciona o valor do campo aos parâmetros da consulta
-            }
-        });
-
-        // Remover a vírgula final da consulta
-        updateQuery = updateQuery.slice(0, -2);
-
-        // Adicionar a cláusula WHERE para o ID
-        updateQuery += ' WHERE id=?'; // Adiciona a cláusula WHERE com o ID do usuário
-        updateParams.push(user.id); // Adiciona o ID do usuário aos parâmetros da consulta
-
-        // Executa a consulta SQL de atualização com os parâmetros fornecidos
-        await db.run(updateQuery, updateParams);
-
-        // Responde com um status de sucesso
-        res.json({
-            "statusCode": 200
-        });
-    } catch (error) {
-        // Em caso de erro, registra o erro no console e responde com um erro interno do servidor
-        console.error("Error updating user:", error);
-        res.status(500).json({ error: "Internal server error" });
+  // Loop pelos campos que podem ser atualizados
+  fieldsToUpdate.forEach((field) => {
+    if (user[field] !== undefined) {
+      updateQuery += `${field}=?, `; // Adiciona o campo à consulta SQL
+      updateParams.push(user[field]); // Adiciona o valor do campo aos parâmetros da consulta
     }
+  });
+
+  // Remover a vírgula final da consulta
+  updateQuery = updateQuery.slice(0, -2);
+
+  // Adicionar a cláusula WHERE para o ID
+  updateQuery += " WHERE id=?"; // Adiciona a cláusula WHERE com o ID do usuário
+  updateParams.push(user.id); // Adiciona o ID do usuário aos parâmetros da consulta
+
+  // Executa a consulta SQL de atualização com os parâmetros fornecidos
+  db.run(updateQuery, updateParams);
 }
 
+export async function getUsers(req, res) {
+  const db = await openDB();
 
-
-export async function getUsers(req,res){
-    try {
-      const db = await openDB();
-      
-      const { users } = await db.all('SELECT* FROM User').then(users=>res.json(users));
-
-      return users;
-        
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+  return db.all("SELECT* FROM User");
 }
 
-export async function getUserById(req,res){
-    try {
-        let id = req.body.id;
-        const db = await openDB();
+export async function getUserById(req, res) {
+  const db = await openDB();
+  const id = req.params.id;
 
-        const { user } = db.all('SELECT* FROM User WHERE id=?', [id])
-        .then(user=>res.json(user));
-
-        return user;
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
+  const [user] = await db.all("SELECT* FROM User WHERE id=?", [id]);
+  return user;
 }
 
-export async function getUserByEmail(email){
-    try {
-        const db = await openDB();
-        const [user] = await db.all('SELECT * FROM User WHERE email=?', [email]);
-    
-        return user; 
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" }); 
-    }
-    
+export async function getUserByEmail(email) {
+  const db = await openDB();
+  const [user] = await db.all("SELECT * FROM User WHERE email=?", [email]);
+
+  return user;
 }
 
-export async function deleteUser(req,res){
-    try {
-        let id = req.body.id;
-        const db = await openDB();
-        await db.all('DELETE FROM User WHERE id=?', [id]);
+export async function deleteUser(req, res) {
+  let id = req.body.id;
+  const db = await openDB();
 
-        res.json({
-            "statusCode": 200
-        });
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Internal server error" });  
-    }
-    
+  db.all("DELETE FROM User WHERE id=?", [id]);
 }
 
-export async function deleteTable(req,res){
-    openDB()
-    .then(db=>{
-        db.all('DROP TABLE IF EXISTS User')
-    });
-}
+export async function deleteTable(req, res) {
+  const db = await openDB();
 
+  db.all("DROP TABLE IF EXISTS User");
+}
--- src/routes.js
+++ src/routes.js
@@ -1,298 +1,366 @@
 import { Router } from "express";
-import { body, validationResult } from 'express-validator';
-import { createUser, updateUser, getUsers, getUserById, getUserByEmail, deleteUser} from "./Controller/User.js"
-import { createProduct, updateProduct, getProducts, getProductById , deleteProduct} from "./Controller/Product.js";
-import { createFavoriteProduct, updateFavoriteProduct, getFavoriteProducts, getFavoriteProductById, getProductIdsOfFavoriteProductsByUserId, getIdFavoriteProductByProductIdAndUserId , deleteFavortiteProduct } from "./Controller/FavoriteProduct.js";
-
-
-
+import { body, param, validationResult } from "express-validator";
+import {
+  createUser,
+  updateUser,
+  getUsers,
+  getUserById,
+  getUserByEmail,
+  deleteUser,
+} from "./Controller/User.js";
+import {
+  createProduct,
+  updateProduct,
+  getProducts,
+  getProductById,
+  deleteProduct,
+} from "./Controller/Product.js";
+import {
+  createFavoriteProduct,
+  updateFavoriteProduct,
+  getFavoriteProducts,
+  getFavoriteProductById,
+  getProductIdsOfFavoriteProductsByUserId,
+  getIdFavoriteProductByProductIdAndUserId,
+  deleteFavortiteProduct,
+} from "./Controller/FavoriteProduct.js";
 
 const router = Router();
 
 //app
-router.get('/', (req,res)=> {
-    res.send("Hello, World!")
+router.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 //user
-router.get('/getUsers', getUsers);
+router.get("/getUsers", getUsers);
 router.get(
-    '/getUserById',
-    [
-        body('id').notEmpty().withMessage('Id de usuário é obrigatório.'), 
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-    
-        getUserById(req, res);
-      }
+  "/getUserById/:id",
+  [param("id").notEmpty().withMessage("Id de usuário é obrigatório.")],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await getUserById(req, res);
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 );
 
 router.get(
-    '/getUserByEmail',
-    [
-        body('email').isEmail().withMessage('Email é obrigatório.'), 
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-    
-        getUserByEmail(req, res);
-      }
+  "/getUserByEmail",
+  [body("email").isEmail().withMessage("Email é obrigatório.")],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await getUserByEmail(req.body.email);
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 );
 
 router.post(
-    '/createUser',
-    [
-      body('name').notEmpty().withMessage('Nome de usuário é obrigatório.'),
-      body('email').isEmail().withMessage('Digite um email válido.').custom(async (value) => {
+  "/createUser",
+  [
+    body("name").notEmpty().withMessage("Nome de usuário é obrigatório."),
+    body("email")
+      .isEmail()
+      .withMessage("Digite um email válido.")
+      .custom(async (value) => {
         const user = await getUserByEmail(value);
 
-        if (!!user) throw new Error("E-mail já cadastrado. Por favor, insira um novo.");
+        if (!!user)
+          throw new Error("E-mail já cadastrado. Por favor, insira um novo.");
         return true;
-      }), 
-      body('phone').notEmpty().withMessage('Telefone é obrigatório.'),
-      body('address').notEmpty().withMessage('Endereço é obrigatório.'),
-      body('password').notEmpty().withMessage('A senha é obrigatória').isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres.'), 
+      }),
+    body("phone").notEmpty().withMessage("Telefone é obrigatório."),
+    body("address").notEmpty().withMessage("Endereço é obrigatório."),
+    body("password")
+      .notEmpty()
+      .withMessage("A senha é obrigatória")
+      .isLength({ min: 6 })
+      .withMessage("A senha deve ter pelo menos 6 caracteres."),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
-    ],
-    (req, res) => {
-      const errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      createUser(req, res);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
+
+    try {
+      createUser(req, res);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
 
 router.put(
-    '/updateUser',
-    [
-        body('name').optional().notEmpty().withMessage('Nome de usuário não pode ser nulo.'),
-        body('email').optional().isEmail().withMessage('Digite um email válido.').custom(async (value) => {
-            const user = await getUserByEmail(value);
-    
-            if (!!user && user.id.toString() !== body('id')){
-             throw new Error("E-mail já cadastrado. Por favor, insira um novo.");
-            }
-            return true;
-        }), 
-        body('phone').optional().notEmpty().withMessage('Telefone não poder ser nulo.'),
-        body('address').optional().notEmpty().withMessage('Endereço não pode ser nulo.'),
-        body('password').optional().notEmpty().withMessage('A senha é obrigatória').isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres.'),
-  
-      ],
-      (req, res) => {
-        const errors = validationResult(req);
-        const a = req.body;
-        console.log({a});
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
+  "/updateUser",
+  [
+    body("name")
+      .optional()
+      .notEmpty()
+      .withMessage("Nome de usuário não pode ser nulo."),
+    body("email")
+      .optional()
+      .isEmail()
+      .withMessage("Digite um email válido.")
+      .custom(async (value) => {
+        const user = await getUserByEmail(value);
+
+        if (!!user && user.id.toString() !== body("id")) {
+          throw new Error("E-mail já cadastrado. Por favor, insira um novo.");
         }
-    
-        updateUser(req, res);
-      }
+        return true;
+      }),
+    body("phone")
+      .optional()
+      .notEmpty()
+      .withMessage("Telefone não poder ser nulo."),
+    body("address")
+      .optional()
+      .notEmpty()
+      .withMessage("Endereço não pode ser nulo."),
+    body("password")
+      .optional()
+      .notEmpty()
+      .withMessage("A senha é obrigatória")
+      .isLength({ min: 6 })
+      .withMessage("A senha deve ter pelo menos 6 caracteres."),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    const a = req.body;
+    console.log({ a });
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      updateUser(req, res);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 );
 
 router.delete(
-    '/deleteUser',
-    [
-        body('id').notEmpty().withMessage('Id de usuário é obrigatório.'), 
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-    
-        deleteUser(req, res);
-    }  
+  "/deleteUser",
+  [body("id").notEmpty().withMessage("Id de usuário é obrigatório.")],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      deleteUser(req, res);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 );
 
 //product
-router.get('/getProducts', getProducts);
+router.get("/getProducts", getProducts);
 router.get(
-    '/getProductById',
-    [
-        body('id').notEmpty().withMessage('Id de produto é obrigatório.'), 
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-    
-        getProductById(req, res);
-    }  
+  "/getProductById",
+  [body("id").notEmpty().withMessage("Id de produto é obrigatório.")],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    getProductById(req, res);
+  }
 );
 
 router.post(
-    '/createProduct',
-    [
-        body('name').notEmpty().withMessage('Nome do produto é obrigatório.'),
-        body('price').notEmpty().withMessage('Preço do produto é obrigatório.'),
-        body('image').notEmpty().withMessage('Imagem do produto é obrigatório.') 
-    ],
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        createProduct(req, res); 
-    } 
+  "/createProduct",
+  [
+    body("name").notEmpty().withMessage("Nome do produto é obrigatório."),
+    body("price").notEmpty().withMessage("Preço do produto é obrigatório."),
+    body("image").notEmpty().withMessage("Imagem do produto é obrigatório."),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    createProduct(req, res);
+  }
 );
 
 router.put(
-    '/updateProduct',
-    [
-        body('name').optional().notEmpty().withMessage('Nome do produto não pode ser nulo.'),
-        body('price').optional().notEmpty().withMessage('Preço do produto não pode ser nulo.'),
-        body('image').optional().notEmpty().withMessage('Imagem do produto não pode ser nulo.') 
-    ],
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        updateProduct(req, res); 
-    } 
-     );
+  "/updateProduct",
+  [
+    body("name")
+      .optional()
+      .notEmpty()
+      .withMessage("Nome do produto não pode ser nulo."),
+    body("price")
+      .optional()
+      .notEmpty()
+      .withMessage("Preço do produto não pode ser nulo."),
+    body("image")
+      .optional()
+      .notEmpty()
+      .withMessage("Imagem do produto não pode ser nulo."),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    updateProduct(req, res);
+  }
+);
 
 router.delete(
-    '/deleteProduct',
-    [
-        body('id').notEmpty().withMessage('Id de produto é obrigatório.'), 
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-    
-        deleteProduct(req, res);
-    } 
+  "/deleteProduct",
+  [body("id").notEmpty().withMessage("Id de produto é obrigatório.")],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    deleteProduct(req, res);
+  }
 );
 
 //favoriteProduct
-router.get('/getFavoriteProducts', getFavoriteProducts);
+router.get("/getFavoriteProducts", getFavoriteProducts);
 router.get(
-    '/getFavoriteProductById',
-    [
-        body('id').notEmpty().withMessage('A id é obrigatória.') 
-    ],
+  "/getFavoriteProductById",
+  [body("id").notEmpty().withMessage("A id é obrigatória.")],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        getFavoriteProductById(req, res); 
-    }     
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    getFavoriteProductById(req, res);
+  }
 );
 router.get(
-    '/getProductIdsOfFavoriteProductsByUserId',
-    [
-        body('userId').notEmpty().withMessage('A id do usuário é obrigatória.') 
-    ],
+  "/getProductIdsOfFavoriteProductsByUserId",
+  [body("userId").notEmpty().withMessage("A id do usuário é obrigatória.")],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        getProductIdsOfFavoriteProductsByUserId(req, res); 
-    }    
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    getProductIdsOfFavoriteProductsByUserId(req, res);
+  }
 );
 router.get(
-    '/getIdFavoriteProductByProductIdAndUserId',
-    [
-        body('userId').notEmpty().withMessage('A id do usuário é obrigatória.'),
-        body('productId').notEmpty().withMessage('A id do produto é obrigatória.') 
-    ],
+  "/getIdFavoriteProductByProductIdAndUserId",
+  [
+    body("userId").notEmpty().withMessage("A id do usuário é obrigatória."),
+    body("productId").notEmpty().withMessage("A id do produto é obrigatória."),
+  ],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        getIdFavoriteProductByProductIdAndUserId(req, res); 
-    }  
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    getIdFavoriteProductByProductIdAndUserId(req, res);
+  }
 );
 
 router.post(
-    '/createFavoriteProduct',
-    [
-        body('userId').notEmpty().withMessage('A id do usuário é obrigatória.'),
-        body('productId').notEmpty().withMessage('A id do produto é obrigatória.') 
-    ],
+  "/createFavoriteProduct",
+  [
+    body("userId").notEmpty().withMessage("A id do usuário é obrigatória."),
+    body("productId").notEmpty().withMessage("A id do produto é obrigatória."),
+  ],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        createFavoriteProduct(req, res); 
-    }     
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    createFavoriteProduct(req, res);
+  }
 );
 
 router.put(
-    '/updateFavoriteProduct',
-    [
-        body('userId').optional().notEmpty().withMessage('A id do usuário não pode ser nula.'),
-        body('productId').optional().notEmpty().withMessage('A id do produto não pode ser nula.') 
-    ],
+  "/updateFavoriteProduct",
+  [
+    body("userId")
+      .optional()
+      .notEmpty()
+      .withMessage("A id do usuário não pode ser nula."),
+    body("productId")
+      .optional()
+      .notEmpty()
+      .withMessage("A id do produto não pode ser nula."),
+  ],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        updateFavoriteProduct(req, res); 
-    }  
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    updateFavoriteProduct(req, res);
+  }
 );
 
 router.delete(
-    '/deleteFavoriteProduct', 
-    [
-        body('id').notEmpty().withMessage('A id é obrigatória.') 
-    ],
+  "/deleteFavoriteProduct",
+  [body("id").notEmpty().withMessage("A id é obrigatória.")],
 
-    (req,res) => {
-        const errors = validationResult(req);
-  
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        
-        deleteFavortiteProduct(req, res); 
-    }  
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    deleteFavortiteProduct(req, res);
+  }
 );
 
 export default router;
